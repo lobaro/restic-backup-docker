531855c7ecb28718111b905445e2edd88785e652
lobaro-demo <info@lobaro.de>
2017-01-17 16:12:07 +0000

    Remove redundant echo to cron log

--- backup.sh
+++ backup.sh
@@ -1,24 +1,27 @@
 #!/bin/sh
 
-echo "Starting Backup" >> /var/log/cron.log
+echo "Starting Backup"
 
+# Do not save full backup log to logfile but to backup-last.log
 restic backup /data --tag=${RESTIC_TAG?"Missing environment variable RESTIC_TAG"} > /var/log/backup-last.log 2>&1
 rc=$?
+echo "Finished backup at $(date)" >> /var/log/backup-last.log
 if [[ $rc == 0 ]]; then
-    echo "Backup Successfull" >> /var/log/cron.log
+    echo "Backup Successfull" 
 else
-    echo "Backup Failed with Status ${rc}" >> /var/log/cron.log
-    restic unlock >> /var/log/cron.log 2>&1
+    echo "Backup Failed with Status ${rc}"
+    restic unlock
 fi
 
 if [ -n "${RESTIC_FORGET_ARGS}" ]; then
-    echo "Forget about old snapshots based on RESTIC_FORGET_ARGS = ${RESTIC_FORGET_ARGS}" >> /var/log/cron.log
+    echo "Forget about old snapshots based on RESTIC_FORGET_ARGS = ${RESTIC_FORGET_ARGS}"
     restic forget ${RESTIC_FORGET_ARGS} >> /var/log/backup-last.log 2>&1
     rc=$?
+    echo "Finished forget at $(date)" >> /var/log/backup-last.log
     if [[ $rc == 0 ]]; then
-        echo "Forget Successfull" >> /var/log/cron.log
+        echo "Forget Successfull"
     else
-        echo "Forget Failed with Status ${rc}" >> /var/log/cron.log
-        restic unlock >> /var/log/cron.log 2>&1
+        echo "Forget Failed with Status ${rc}"
+        restic unlock
     fi
 fi
