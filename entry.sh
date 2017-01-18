bd146dbf3b521f4c507001634e480f9e024b21f0
lobaro-demo <info@lobaro.de>
2017-01-18 10:45:39 +0000

    Improove logs

--- entry.sh
+++ entry.sh
@@ -4,16 +4,16 @@ set -e
 echo "Starting container ..."
 
 if [ -n "${NFS_TARGET}" ]; then
-    echo "Mounting NFS based on NFS_TARGET"
+    echo "Mounting NFS based on NFS_TARGET: ${NFS_TARGET}"
     mount -o nolock -v ${NFS_TARGET} /mnt/restic
 fi
 
 if [ ! -f "$RESTIC_REPOSITORY/config" ]; then
-    echo "Restic repository does not exists. Running restic init."
+    echo "Restic repository '${RESTIC_REPOSITORY}' does not exists. Running restic init."
     restic init | true
 fi
 
-echo "Setup backup cron job with cron expression: ${BACKUP_CRON}"
+echo "Setup backup cron job with cron expression BACKUP_CRON: ${BACKUP_CRON}"
 echo "${BACKUP_CRON} /bin/backup >> /var/log/cron.log 2>&1" > /var/spool/cron/crontabs/root
 
 # Make sure the file exists before we start tail
