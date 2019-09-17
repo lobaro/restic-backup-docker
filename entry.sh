6bd261d88780cd4d1638933014636a3f1a0b9750
Tim Collins <tim.collins@worldprogramming.com>
2019-09-17 10:54:08 +0100

    Enable the entrypoint to check for sftp remote repository and create if necessary

--- entry.sh
+++ entry.sh
@@ -1,4 +1,4 @@
-#!bin/sh
+#!bin/bash
 
 echo "Starting container ..."
 
@@ -13,24 +13,35 @@ if [ -n "${NFS_TARGET}" ]; then
     mount -o nolock -v ${NFS_TARGET} /mnt/restic
 fi
 
-restic snapshots &>/dev/null
-status=$?
-echo "Check Repo status $status"
+if grep -q 'sftp:' $RESTIC_REPOSITORY; then
+        host_regex='^sftp:[^\s]+@([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+):(.+)$'
+        [[ $RESTIC_REPOSITORY =~ $host_regex ]]
 
-if [ $status != 0 ]; then
-    echo "Restic repository '${RESTIC_REPOSITORY}' does not exist. Running restic init."
-    restic init
+        # Test whether the path exists on the remote repository. If not then create it.
+        if ssh ${BASH_REMATCH[1]} stat ${BASH_REMATCH[2]} \> /dev/null 2\>\&1; then
+                echo "Successfully found Restic repository '${RESTIC_REPOSITORY}'."
+            else
+                echo "Restic repository '${RESTIC_REPOSITORY}' does not exist. Running restic init."
+                restic init | true
+        fi
+    else
+        restic snapshots &>/dev/null
+        status=$?
+        echo "Check Repo status $status"
 
-    init_status=$?
-    echo "Repo init status $init_status"
+        if [ $status != 0 ]; then
+            echo "Restic repository '${RESTIC_REPOSITORY}' does not exist. Running restic init."
+            restic init
 
-    if [ $init_status != 0 ]; then
-        echo "Failed to init the repository: '${RESTIC_REPOSITORY}'"
-        exit 1
-    fi
+            init_status=$?
+            echo "Repo init status $init_status"
+
+            if [ $init_status != 0 ]; then
+                echo "Failed to init the repository: '${RESTIC_REPOSITORY}'"
+                exit 1
+            fi
+        fi
 fi
-
-
 
 echo "Setup backup cron job with cron expression BACKUP_CRON: ${BACKUP_CRON}"
 echo "${BACKUP_CRON} /bin/backup >> /var/log/cron.log 2>&1" > /var/spool/cron/crontabs/root
